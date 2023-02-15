644a08a060276c62158d1a54b5f988db3350ae1d
Eloy Gracia <eloygb2000@gmail.com>
2023-02-15 09:43:47 +0100

    Fixed script

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ service docker start
 systemctl enable docker
 usermod -a -G docker ec2-user
 pip3 install docker-compose
-wget <https://raw.githubusercontent.com/2000GHz/hello-2048/master/docker-compose.yaml>
+wget https://raw.githubusercontent.com/2000GHz/hello-2048/master/docker-compose.yaml
 docker-compose pull
 docker-compose up -d
 EOF
